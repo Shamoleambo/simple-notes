5748284a86ba0737d824f6e2ff6b2c7dd16d60d1
Tiago Gomes <tidgomes@gmail.com>
2023-05-26 16:47:45 -0300

    feat: use dotenv to access environment varibles

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const server = express()
+dotenv.config()
 
-server.listen(3000, () => {
-  console.log(`Server up and running`)
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`)
 })
