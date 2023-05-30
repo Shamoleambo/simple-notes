efeb5115889b93f5411050805f297dc4c4b036da
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 10:32:01 -0300

    feat: use Form component from react-bootstrap

--- frontend/src/Components/NoteForm.jsx
+++ frontend/src/Components/NoteForm.jsx
@@ -1,23 +1,28 @@
+import Form from 'react-bootstrap/Form'
 import classes from './NoteForm.module.css'
 
-const NoteForm = () => {
+const NoteForm = ({ onBackdropClick }) => {
   return (
     <>
-      <div className={classes.backdrop}></div>
-      <form className={classes.form}>
+      <div className={classes.backdrop} onClick={onBackdropClick}></div>
+      <Form className={classes.form}>
         <div className={classes.formControlHeader}>
           <h1>Create Your Note</h1>
           <p>Give it a title and some text</p>
         </div>
-        <div className='form-control'>
-          <label htmlFor='title' name='title'>
-            Title
-          </label>
-          <input id='title' type='text' />
-        </div>
-        <label htmlFor='noteText'>Note Content</label>
-        <textarea id='noteText' rows='4' name='noteText'></textarea>
-      </form>
+        <Form.Group controlId='title' className='mb-4'>
+          <Form.Label>Title</Form.Label>
+          <Form.Control type='text' placeholder='Give your note a title' />
+        </Form.Group>
+        <Form.Group controlId='noteText'>
+          <Form.Label>Note Content</Form.Label>
+          <Form.Control
+            as='textarea'
+            placeholder='Note content'
+            style={{ height: '10rem' }}
+          />
+        </Form.Group>
+      </Form>
     </>
   )
 }
