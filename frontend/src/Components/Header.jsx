252c89a9cab7ce2599cb40c85eb9e1dd7ec64dc6
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 09:37:30 -0300

    feat: use bootstrap Button in Header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,10 +1,13 @@
+import Button from 'react-bootstrap/Button'
 import classes from './Header.module.css'
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <h1 className={classes.title}>Simple Notes</h1>
-      <button>Create Note</button>
+      <Button variant='outline-primary' size='lg'>
+        Create Note
+      </Button>
     </header>
   )
 }
