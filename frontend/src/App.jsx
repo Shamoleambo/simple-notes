1dac376e9227161152d4ca20e24f1b6a804ce623
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 10:08:13 -0300

    feat: remove NoteForm component from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './Components/Header'
-import NoteForm from './Components/NoteForm'
 
 function App() {
   return (
     <main>
       <Header />
-      <NoteForm />
     </main>
   )
 }
