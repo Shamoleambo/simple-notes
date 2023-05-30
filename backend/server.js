c2fe2466f0f4cbc5977e9742b926d109554b3c33
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 11:08:41 -0300

    feat: create middleware to allow cors

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import notesRoutes from './routes/notesRoutes.js'
 import connectDB from './config/db.js'
+import { allowCors } from './middleware/allowCors.js'
 
 const server = express()
 dotenv.config()
 connectDB()
 
 const PORT = process.env.PORT || 5000
+
+server.use(allowCors)
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
