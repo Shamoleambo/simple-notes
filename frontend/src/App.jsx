7f5f19a728549ebac0d0a28c92ad798510a74e20
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 09:07:24 -0300

    feat: create Header component and render it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './Components/Header'
 import NoteForm from './Components/NoteForm'
 
 function App() {
   return (
     <>
+      <Header />
       <NoteForm />
     </>
   )
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Simple Notes</h1>
+      <button>Create Note</button>
+    </header>
+  )
+}
+
+export default Header
