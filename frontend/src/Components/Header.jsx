c38f8381ba2f68ed3ef9ce941841a62c74172d8a
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 10:09:40 -0300

    feat: make Header component into a controlled component and render NoteForm conditionally

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
+import NoteForm from './NoteForm'
 import classes from './Header.module.css'
 
 const Header = () => {
+  const [formIsVisible, setFormIsVisible] = useState(false)
+
+  function handleFormVisible() {
+    setFormIsVisible(true)
+  }
+
+  function handleFormInvisible() {
+    setFormIsVisible(false)
+  }
+
   return (
-    <header className={classes.header}>
-      <h1 className={classes.title}>Simple Notes</h1>
-      <Button variant='outline-primary' size='lg'>
-        Create Note
-      </Button>
-    </header>
+    <>
+      <header className={classes.header}>
+        <h1 className={classes.title}>Simple Notes</h1>
+        <Button variant='outline-primary' size='lg' onClick={handleFormVisible}>
+          Create Note
+        </Button>
+      </header>
+      {formIsVisible && (
+        <>
+          <NoteForm onBackdropClick={handleFormInvisible} />
+        </>
+      )}
+    </>
   )
 }
 
