dda4937ec1a58e05170f0b2a344732954226bc2f
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 11:11:22 -0300

    feat: set App as a controlled component and fetch the notes from the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './Components/Header'
 import NotesList from './Components/NotesList'
 
 function App() {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const fetchedData = await fetch('http://localhost:5000/notes')
+      const notes = await fetchedData.json()
+      console.log('fetchNotes', fetchNotes)
+      console.log('fetched notes', notes)
+      setNotes(notes)
+    }
+
+    console.log('state', notes)
+
+    fetchNotes()
+  }, [])
   return (
     <main>
       <Header />
