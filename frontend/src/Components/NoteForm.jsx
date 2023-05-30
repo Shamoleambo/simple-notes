b97548bb28f1b3223808ebfa60cc9550c9609088
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 10:10:41 -0300

    chore: create NoteForm.module.css and a backdrop when it being rendered

--- frontend/src/Components/NoteForm.jsx
+++ frontend/src/Components/NoteForm.jsx
@@ -1,17 +1,24 @@
+import classes from './NoteForm.module.css'
+
 const NoteForm = () => {
   return (
-    <form>
-      <h1>Create Your Note</h1>
-      <p>Give it a title and some text</p>
-      <div className='form-control'>
-        <label htmlFor='title' name='title'>
-          Title
-        </label>
-        <input id='title' type='text' />
-      </div>
-      <label htmlFor='noteText'>Note Content</label>
-      <textarea id='noteText' rows='4' name='noteText'></textarea>
-    </form>
+    <>
+      <div className={classes.backdrop}></div>
+      <form className={classes.form}>
+        <div className={classes.formControlHeader}>
+          <h1>Create Your Note</h1>
+          <p>Give it a title and some text</p>
+        </div>
+        <div className='form-control'>
+          <label htmlFor='title' name='title'>
+            Title
+          </label>
+          <input id='title' type='text' />
+        </div>
+        <label htmlFor='noteText'>Note Content</label>
+        <textarea id='noteText' rows='4' name='noteText'></textarea>
+      </form>
+    </>
   )
 }
 
