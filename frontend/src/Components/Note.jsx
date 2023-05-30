9bce841fe383ec2b4a1d67a4c36a076eab511c46
Tiago Gomes <tidgomes@gmail.com>
2023-05-30 15:24:50 -0300

    feat: create Note.modules.css for styling

--- frontend/src/Components/Note.jsx
+++ frontend/src/Components/Note.jsx
@@ -1,8 +1,10 @@
+import classes from './Note.module.css'
+
 const Note = ({ title, noteText }) => {
   return (
-    <li>
-      <h1>{title}</h1>
-      <p>{noteText}</p>
+    <li className={classes.noteItem}>
+      <h1 className={classes.noteTitle}>{title}</h1>
+      <p className={classes.noteContent}>{noteText}</p>
     </li>
   )
 }
